86ef424e242479df8ff0150f8f8d2f33c69b947b
david nakayama <david.m.nakayama@vanderbilt.edu>
2017-11-19 20:20:39 -0800

    Change whitespace to 2 spaces.

--- app/screens/twitter-signin/index.js
+++ app/screens/twitter-signin/index.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableHighlight, Linking, AsyncStorage } from 'react-native';
-import twitter, { auth } from 'react-native-twitter';
+import { View, Text, TouchableHighlight, Linking, AsyncStorage } from 'react-native';
+import { auth } from 'react-native-twitter';
 
 import config from '../../config';
 import styles from './styles';
 
 export default class TwitterSignin extends Component {
-    state = {
-        login: '',
-        password: '',
-        showPassword: false
-    }
+  state = {
+    login: '',
+    password: '',
+    showPassword: false,
+  }
 
-    componentDidMount() {
-        Linking.canOpenURL('socialauth://twitter').then((supported) => { console.warn(supported); })
-    }
+  componentDidMount() {
+    Linking.canOpenURL('socialauth://twitter').then((supported) => { console.warn(supported); });
+  }
 
-    signIn = async () => {
-        try {
-            const twitterResponse = await auth(
+  signIn = async () => {
+    try {
+      const twitterResponse = await auth(
                 { consumerKey: config.twitterConsumerToken, consumerSecret: config.twitterConsumerSecret },
-                'socialauth://twitter'
+                'socialauth://twitter',
             );
-            console.warn(twitterResponse.id);
-            AsyncStorage.setItem('twitterTokens', JSON.stringify(twitterResponse));
-        } catch(error) {
-            console.warn('error');
-            console.warn(error);
-        };
+      console.warn(twitterResponse.id);
+      AsyncStorage.setItem('twitterTokens', JSON.stringify(twitterResponse));
+    } catch (error) {
+      console.warn('error');
+      console.warn(error);
     }
+  }
 
-    handleSignInResult = (event) => {
-        console.log(event);
-        this.setState({ supported: 'some value' });
-    }
+  handleSignInResult = (event) => {
+    console.log(event);
+    this.setState({ supported: 'some value' });
+  }
 
-    render() {
-        const { login, password, showPassword, supported } = this.state;
-        return (
-            <View>
-                <Text style={supported ? { color: 'red' } : { color: 'blue' }}>Twitter</Text>
-                <Text>{`${supported}`}</Text>
-                <TouchableHighlight onPress={this.signIn} style={styles.signInButton}>
-                    <Text>Sign In With Twitter</Text>
-                </TouchableHighlight>
-            </View>
-        );
-    }
+  render() {
+    const { supported } = this.state;
+    return (
+      <View>
+        <Text style={supported ? { color: 'red' } : { color: 'blue' }}>Twitter</Text>
+        <Text>{`${supported}`}</Text>
+        <TouchableHighlight onPress={this.signIn} style={styles.signInButton}>
+          <Text>Sign In With Twitter</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
