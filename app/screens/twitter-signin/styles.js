df24e34e240b75012427c02bf50fb4e7c7d24c05
imundy <mundycore@gmail.com>
2017-11-19 19:10:06 -0800

    Add initial twitter signin work

---
+++ app/screens/twitter-signin/styles.js
@@ -0,0 +1,13 @@
+import { Dimensions } from 'react-native';
+
+const styles = {
+    signInButton: {
+        width: Dimensions.get('screen').width - 32,
+        height: 30,
+        marginVertical: 16,
+        borderWidth: 1,
+        borderColor: '#CCC'
+    }
+};
+
+export default styles;
